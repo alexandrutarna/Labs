4d4a85e9831f46f77e27c3e5333043626830af25
Alexandru Tarna <tarna.alexandru@gmail.com>
2018-04-27 11:20:48 +0200

    Initial commit

---
+++ app/src/main/java/it/polito/mad/lab1/book/manager/AddBookActivity.java
@@ -0,0 +1,98 @@
+package it.polito.mad.lab1.book.manager;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import it.polito.mad.lab1.R;
+
+public class AddBookActivity extends AppCompatActivity
+        implements View.OnClickListener {
+
+    // Variables for the search input field, and results TextViews.
+    private EditText mIsbnInput;
+    private TextView mTitleText;
+    private TextView mAuthorText;
+
+
+
+
+    /**
+     * Standard Activity lifecycle methods
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_book);
+
+        mIsbnInput = (EditText)findViewById(R.id.isbn_input);
+        mTitleText = (TextView)findViewById(R.id.titleText);
+        mAuthorText = (TextView)findViewById(R.id.authorText);
+
+
+        findViewById(R.id.searchBookButton).setOnClickListener(this);
+
+
+
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.searchBookButton:
+                searchBooks();
+                break;
+        }
+
+    }
+
+
+    /**
+     * Gets called when the user pushes the "Search Books" button
+     */
+    public void searchBooks() {
+        // Get the search string from the input field.
+        String queryString = mIsbnInput.getText().toString();
+
+        // Hide the keyboard when the button is pushed.
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
+
+        // Check the status of the network connection.
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If the network is active and the search field is not empty, start a FetchBook AsyncTask.
+        if (networkInfo != null && networkInfo.isConnected() && queryString.length()!=0) {
+//            new FetchBook(mTitleText, mAuthorText, mBookInput).execute(queryString);
+
+
+
+            new FetchBookByIsbn(mTitleText, mAuthorText, mIsbnInput).execute(queryString);
+
+
+        }
+        // Otherwise update the TextView to tell the user there is no connection or no search term.
+        else {
+            if (queryString.length() == 0) {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_search_term);
+            } else {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_network);
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/it/polito/mad/lab1/book/manager/Book.java
@@ -0,0 +1,91 @@
+/*
+ Thiss class is not used yed, it was created for storing a book object in order to ease the
+ manipulation for a book.
+ */
+
+
+
+package it.polito.mad.lab1.book.manager;
+
+public class Book {
+
+
+    private String bookTitle;
+    private String bookAuthor;
+    private String bookISBN;
+    private String bookPublisher;
+    private String bookEditionYear;
+
+    private String extraDeails = null;
+
+    // constructor without extra details
+    public Book(String bookTitle, String bookAuthor, String bookISBN, String bookPublisher, String bookEditionYear) {
+        this.bookTitle = bookTitle;
+        this.bookAuthor = bookAuthor;
+        this.bookISBN = bookISBN;
+        this.bookPublisher = bookPublisher;
+        this.bookEditionYear = bookEditionYear;
+    }
+
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public String getBookISBN() {
+        return bookISBN;
+    }
+
+    public void setBookISBN(String bookISBN) {
+        this.bookISBN = bookISBN;
+    }
+
+    public String getBookPublisher() {
+        return bookPublisher;
+    }
+
+    public void setBookPublisher(String bookPublisher) {
+        this.bookPublisher = bookPublisher;
+    }
+
+    public String getBookEditionYear() {
+        return bookEditionYear;
+    }
+
+    public void setBookEditionYear(String bookEditionYear) {
+        this.bookEditionYear = bookEditionYear;
+    }
+
+
+    public String getExtraDeails() {
+        return extraDeails;
+    }
+
+    public void setExtraDeails(String extraDeails) {
+        this.extraDeails = extraDeails;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookTitle='" + bookTitle + '\'' +
+                ", bookAuthor='" + bookAuthor + '\'' +
+                ", bookISBN='" + bookISBN + '\'' +
+                ", bookPublisher='" + bookPublisher + '\'' +
+                ", bookEditionYear='" + bookEditionYear + '\'' +
+                ", extraDeails='" + extraDeails + '\'' +
+                '}';
+    }
+}
